53e774914e8d8e81811791f06c4cb0348ad0c196
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2023-11-14 17:53:56 +0900

    [Silver III] Title: 2×n 타일링, Time: 124 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -8,7 +8,7 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
 
-        long[] dp = new long[1001];
+        int[] dp = new int[1001];
         for(int i=0; i<=n; i++){
             if(i==1) dp[i] = 1;
             if(i==2) dp[i] = 2;
