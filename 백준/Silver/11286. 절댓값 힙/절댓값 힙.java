5867d0eb2bec594ac8a687c254941419b6d7b982
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-01-18 14:09:26 +0900

    [Silver I] Title: 절댓값 힙, Time: 628 ms, Memory: 27204 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,35 +1,42 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    static class Num{
+        int isPositive;
+        int num;
 
+        public Num(int isPositive, int num){
+            this.isPositive = isPositive;
+            this.num = num;
+        }
+    }
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> {
-            // 절댓값이 작은 데이터 우선
-            // 절댓값이 같은 경우 음수 우선
-            int first_abs = Math.abs(o1);
-            int second_abs = Math.abs(o2);
-
-            if(first_abs == second_abs){
-                return o1 > o2 ? 1 : -1;
+        PriorityQueue<Num> pq = new PriorityQueue<>(new Comparator<Num>() {
+            @Override
+            public int compare(Num o1, Num o2) {
+                if(o1.num > o2.num) return 1;
+                else if(o1.num < o2.num) return -1;
+                else return o1.isPositive > o2.isPositive ? 1 : -1;
             }
-            return first_abs - second_abs;
         });
 
         for(int i=0; i<N; i++){
-            int request = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
 
-            if(request == 0) {
-                if(queue.isEmpty()) System.out.println("0");
-                else System.out.println(queue.poll());
+            if(n == 0) {
+                if(pq.isEmpty()) System.out.println(0);
+                else{
+                    Num thisNumber = pq.poll();
+                    System.out.println(thisNumber.num * thisNumber.isPositive);
+                }
             }
             else {
-                queue.add(request);
+                if (n > 0) pq.add(new Num(1, n));
+                else pq.add(new Num(-1, n * (-1)));
             }
         }
     }
