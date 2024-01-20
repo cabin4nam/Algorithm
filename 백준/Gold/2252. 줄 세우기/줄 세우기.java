eeea15adb7091e64a78c7c78f7d3009821215c92
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-01-20 18:41:21 +0900

    [Gold III] Title: 줄 세우기, Time: 812 ms, Memory: 44212 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,55 +1,71 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
+// 이 코드가 왜 틀렸는지 모르겠다...
+
+/**
+ * 반례 ::
+ *
+ * 입력
+ * 5 4
+ * 2 3
+ * 1 2
+ * 4 2
+ * 5 2
+ *
+ * 정답
+ * 1 4 5 2 3
+ */
 public class Main {
+    static ArrayList<Integer>[] students; // 그래프를 표현할 자료구조
+    static int[] indegree;// 진입차수를 저장할 자료구조
+    static boolean[] visited; // 방문 검사 배열
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        ArrayList<ArrayList<Integer>> students = new ArrayList<>(); // 학생들 정보 저장할 인접 리스트 
-        for(int i=0; i<=N; i++){ // 초기화
-            ArrayList<Integer> node = new ArrayList<>();
-            students.add(node);
+        students = new ArrayList[N+1];
+        for(int i=0; i< students.length; i++){
+            students[i] = new ArrayList<>();
         }
-        
-        int[] indegree = new int[N+1]; // 학생들 노드의 진입차수 배열
-        for(int i=1; i<=N; i++) indegree[i] = 0; // 초기화
+        indegree = new int[N+1];
+        Arrays.fill(indegree, 0);
+        visited = new boolean[N+1];
 
         for(int i=0; i<M; i++){
             st = new StringTokenizer(br.readLine());
-            int s1 = Integer.parseInt(st.nextToken());
-            int s2 = Integer.parseInt(st.nextToken());
+            int beforeStudent = Integer.parseInt(st.nextToken());
+            int afterStudent = Integer.parseInt(st.nextToken());
 
-            // 학생들의 선후 관계 정보 -> 인접 리스트에 저장
-            students.get(s1).add(s2);
-
-            // 진입 차수 배열 업데이트
-            indegree[s2] ++;
+            indegree[afterStudent] ++;
+            students[beforeStudent].add(afterStudent);
         }
 
-        Queue<Integer> queue = new LinkedList<>();
-
-        // 진입 차수가 0인 학생(노드)를 큐에 먼저 저장 (초기 세팅)
         for(int i=1; i<=N; i++){
-            if(indegree[i] == 0) queue.offer(i);
-        }
-        
-        while(!queue.isEmpty()){
-            // 큐에서 학생 하나 꺼내서 출력
-            int student = queue.poll();
-            System.out.print(student + " ");
-
-            // 꺼낸 학생 다음에 올 학생들의 진입차수 하나씩 줄여주기. -> 진입차수가 0이 되는 학생이 있으면 역시 queue에 저장
-            for(int i=0; i<students.get(student).size(); i++){
-                int node = students.get(student).get(i);
-                indegree[node] --;
-                
-                if(indegree[node] == 0) queue.offer(node);
-            }
+//            System.out.println("upper code");
+            if(!visited[i]) makeLine(i);
         }
     }
 
+    static private void makeLine(int index){
+        // 만약 진입차수가 0이면 바로 출력
+
+        if(indegree[index]==0 && !visited[index]){
+            System.out.println(index);
+            visited[index] = true;
+            for(int s : students[index]){
+                if(!visited[s]){
+                    indegree[s] --;
+                    makeLine(s);
+                }
+            }
+        }
+    }
 }
