f453272b038302eaabd3746e7d013e3da859fdb6
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-01-20 19:39:27 +0900

    [level 2] Title: 모음 사전, Time: 2.66 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,34 +1,36 @@
-import java.util.ArrayList;
-import java.util.List;
 class Solution {
-    static String[] index = {"A", "E", "I", "O", "U"};
-    static List<String> list;
+    static boolean[] visited;
+    static int answer = 0;
+    static String[] eng = {"A", "E", "I", "O", "U"};
+    static boolean isFound = false;
     public int solution(String word) {
-        int answer = 0;
         
-        list = new ArrayList<>();
-        // dfs로 사전에 들어있는 모든 문자열 경우의 수 list에 저장 
-        dfs("", 0, word);
+        visited = new boolean[word.length()];
         
-        // list에서 word가 몇 번째에 위치하는 지 찾기
-        for (int i = 0; i < list.size(); i++) {
-            if(list.get(i).equals(word)) {
-                answer = i;
-                break;
-            }
+        for(int i=0; i<5; i++){
+            if(isFound) break;
+            
+            DFS(word, eng[i]);
         }
+        
         return answer;
     }
-   
-    static void dfs(String str, int depth, String word) {
-        list.add(str);
+    
+    private void DFS(String word, String str){
+        if(str.length() > 5) return;
         
-        if(depth == 5) {
+        answer++;
+        if(str.equals(word)){
+            isFound = true;
             return;
-        }
-        
-        for (int i = 0; i < index.length; i++) {
-            dfs(str + index[i], depth + 1, word);
+        } 
+         
+        for(int i=0; i<5; i++){
+            if(isFound) break;
+            
+            DFS(word, str+eng[i]);
+            
         }
     }
+    
 }
