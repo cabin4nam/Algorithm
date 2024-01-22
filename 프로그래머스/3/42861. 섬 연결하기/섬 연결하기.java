453fb561c914148c7d7c5b73cf44e48a0e263ea7
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-01-22 13:49:46 +0900

    [level 3] Title: 섬 연결하기, Time: 0.35 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,24 +1,33 @@
 import java.util.*;
 class Solution {
-    static int[] parent; // 대표 노드(부모 노드)를 저장할 배열
+    static int edge_cnt;
+    static int[] parents;
     public int solution(int n, int[][] costs) {
         int answer = 0;
         
-        // 대표 노드 배열 초기화
-        parent = new int[n];
-        for(int i=0; i<n; i++){
-            parent[i] = i;
+        edge_cnt = 0; // 연결한 엣지 수 (n-1개가 될때까지 반복문을 순환)
+        parents = new int[n]; // 부모 노드를 저장
+        for(int i = 0; i<n; i++){ // 초기 설정은 본인 노드를 부모로 지정
+            parents[i] = i;
         }
         
-        // 크루스칼 알고리즘을 사용하기 위해 가중치 기준 오름차순 정렬 (2번)
-        Arrays.sort(costs, (int[] c1, int[] c2) -> c1[2] - c2[2]);
-        
-        // 가중치가 작은 것부터 (처음부터) 연결 시도
+        // costs를 가중치 기준으로 오름차순 정렬
+        Arrays.sort(costs, new Comparator<int[]>(){
+            @Override
+            public int compare(int[] c1, int[] c2){
+                return c1[2]>=c2[2] ? 1 : -1;
+            }
+        });
+            
         for(int i=0; i<costs.length; i++){
-            // find 연산으로 찾은 대표 노드가 서로 다를때만 연결
-            if(find(costs[i][0]) != find(costs[i][1])){
-                // union연산으로 연결
+            if(edge_cnt == n-1) break;
+            
+            int parent1 = find(costs[i][0]);
+            int parent2 = find(costs[i][1]);
+            
+            if(parent1 != parent2){
                 union(costs[i][0], costs[i][1]);
+                edge_cnt++;
                 answer += costs[i][2];
             }
         }
@@ -27,16 +36,12 @@ class Solution {
     }
     
     private int find(int node){
-        if(parent[node] == node)
-            return node;
+        if(parents[node] == node) return node;
         
-        return parent[node] = find(parent[node]);
+        else return parents[node] = find(parents[node]);
     }
     
     private void union(int node1, int node2){
-        int parent_node1 = find(node1);
-        int parent_node2 = find(node2);
-        
-        parent[parent_node1] = parent_node2;
+        parents[find(node2)] = parents[find(node1)];
     }
 }
