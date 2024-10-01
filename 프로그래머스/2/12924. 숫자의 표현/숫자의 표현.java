2b65508901b7b10497814563f1d6a5878ef3b73f
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-10-02 02:46:52 +0900

    [level 2] Title: 숫자의 표현, Time: 0.84 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,18 +1,29 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
+        
+        // 누적합 배열 생성
+        int[] sum = new int[n+1];
+        for(int i=1; i<=n; i++) sum[i] = sum[i-1] + i;
+        
         int start = 1;
-        int sum = 0;
-        for (int i = 0; i <= n; i++) {
-            sum += i;
-            while (sum > n) {
-                sum -= start;
-                start++;
+        int end = 1;
+        
+        while(start <= n && end <= n && start <= end){
+            int s = sum[end]-sum[start-1];
+            
+            if(s >= n){
+                if(s==n){
+                    answer ++;
+                } 
+                
+                start ++;
             }
-            if (sum == n) {
-                answer++;
+            else {
+                end ++;
             }
         }
+        
         return answer;
     }
 }
