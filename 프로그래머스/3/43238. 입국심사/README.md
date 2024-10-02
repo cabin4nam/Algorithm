fa690fd9b5b6b48b88110f3a547da11edb7eab89
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-10-02 23:30:03 +0900

    [level 3] Title: 입국심사, Time: 135.86 ms, Memory: 99.7 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=java) 
 
 ### 성능 요약
 
-메모리: 73.7 MB, 시간: 0.39 ms
+메모리: 99.7 MB, 시간: 135.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 2일 16:55:12
+2024년 10월 02일 23:30:01
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,36 +1,30 @@
-import java.util.Arrays;
-
+import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
-        long answer = Long.MAX_VALUE;
+        long answer = 0;
         
-        // 이진탐색을 위해 times를 sorting 해준다.
         Arrays.sort(times);
         
-        long left = 0; // n명의 사람을 모두 심사하는 데 걸리는 최소 시간
-        long right = (long)times[times.length -1] * n; //n명의 사람을 모두 심사하는 데 걸리는 최악의 시간 (심사가 오래 걸리는 사람이 모든 사람을 순차적으로 심사)
+        long left = 0;
+        long right = times[times.length-1] * (long)n;
         
         while(left <= right){
-            // 이진탐색에 필요한 mid(중간값) 구하기
             long mid = (left+right)/2;
             
-            // mid 시간동안 심사할 수 있는 사람은 총 몇 명?
-            long sum = 0;
-            // 심사를 빨리 하는 심사관부터 시작해서 각 몇 명을 심사할 수 있는지 sum에 누적
-            for(int i=0; i<times.length; i++){
+            // mid에서 검사할 수 있는 모든 인원 합 구하기
+            long sum=0;
+            for(int i=0; i<times.length; i++)
                 sum += mid/times[i];
-            }
             
-            // sum, 즉 총 심사 가능한 인원 수가 목표 인원수(n) 보다 작으면 -> 시간이 부족함 -> left를 늘려줌
-            if(sum < n){
+            if(sum < n){ // 더 많은 시간이 필요함
                 left = mid+1;
             }
-            // sum, 즉 총 심사 가능한 인원 수가 목표 인원수(n)를 충족하면(같거나 크면) -> 시간 충분함 -> 줄여볼까? -> right를 늘려줌
             else {
+                answer = mid;
                 right = mid-1;
-                answer = answer > mid ? mid : answer;
             }
         }
+        
         return answer;
     }
 }
