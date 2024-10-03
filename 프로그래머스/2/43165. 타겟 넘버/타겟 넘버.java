cc1feefd61740ae839267e4c439c92ec108db21c
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-10-03 21:31:14 +0900

    [level 2] Title: 타겟 넘버, Time: 6.57 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,38 +1,19 @@
-import java.util.*;
 class Solution {
-    static int cnt = 0;
+    private static int answer = 0;
     public int solution(int[] numbers, int target) {
         
-        // 주어진 정수들의 수가 단 한 개일 때
-        if(numbers.length <= 1){
-            // target과 같은 값이면, 단 하나의 경우의 수 가능
-            if(numbers[0] == target) return 1;
-            
-            // target과 다른 값이면, target을 만들 수 있는 경우의 수 없음
-            else return 0;
-        }     
+        dfs(numbers, 0, 0, target);
         
-        // 맨 첫 숫자가 양수일 때 dfs
-        dfs(numbers[0], 1, numbers, target);
-        // 맨 첫 숫자가 음수일 때 dfs
-        dfs(numbers[0] * (-1) , 1, numbers, target);
-        
-        return cnt;
+        return answer;
     }
     
-    public void dfs(int value, int index, int[] numbers, int target){
-        // 모든 수를 사용하여 탐색하였음
-        if(index == numbers.length){
-            // 지금까지 수행한 경우의 수로 인한 결과가 target이면 조합 성공
-            if(value == target) cnt++;
+    public void dfs(int[] numbers, int idx, int sum, int target){
+        if(idx >= numbers.length){
+            if(sum == target) answer++;
             return;
         }
         
-        // 현재까지의 값에 다음 값을 더하여 진행
-        dfs(value + numbers[index], index+1, numbers, target);
-        // 현재까지의 값에 다음 값을 빼서 진행
-        dfs(value - numbers[index], index+1, numbers, target);
-       
-        
+        dfs(numbers, idx+1, sum+numbers[idx], target);
+        dfs(numbers, idx+1, sum-numbers[idx], target);
     }
 }
