0771bacb0a2c496ad81f794fc74f972288870c99
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-10-07 20:34:38 +0900

    [Silver II] Title: DFS와 BFS, Time: 220 ms, Memory: 23648 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    private static int[][] graph;
+    private static boolean[] visited;
+    private static StringBuilder dfsResult = new StringBuilder();
+    private static StringBuilder bfsResult = new StringBuilder();
+    private static int N;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken()); // 정점의 개수
+        int M = Integer.parseInt(st.nextToken()); // 간선의 개수
+        int V = Integer.parseInt(st.nextToken()); // 시작점
+
+        // 그래프 표현
+        graph = new int[N+1][N+1];
+
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int n1 = Integer.parseInt(st.nextToken());
+            int n2 = Integer.parseInt(st.nextToken());
+
+            graph[n1][n2] = graph[n2][n1] = 1;
+        }
+
+        visited = new boolean[N+1];
+        dfs(V);
+
+        visited = new boolean[N+1];
+        bfs(V);
+
+        System.out.println(dfsResult.toString());
+        System.out.println(bfsResult.toString());
+    }
+
+    private static void dfs(int node){
+        visited[node] = true;
+
+        dfsResult.append(node).append(" ");
+
+        for(int next=1; next<=N; next++){
+            if(graph[node][next]==1 && !visited[next]) dfs(next);
+        }
+    }
+
+    private static void bfs(int start){
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        visited[start] = true;
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+            bfsResult.append(node).append(" ");
+
+            for(int next=1; next<=N; next++){
+                if(graph[node][next]==1 && !visited[next]){
+                    visited[next] = true;
+                    q.offer(next);
+                }
+            }
+        }
+    }
+
+}
