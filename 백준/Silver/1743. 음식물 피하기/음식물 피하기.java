9aa4a102a292f19716e82af8712fb532dbd4a395
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-10-16 11:25:33 +0900

    [Silver I] Title: 음식물 피하기, Time: 140 ms, Memory: 16400 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
@@ -1,85 +1,70 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static class Point{
+    private static class Point{
         int r;
         int c;
-
         public Point(int r, int c){
             this.r = r;
             this.c = c;
         }
     }
-
-    static boolean[][] map;
-    static boolean[][] visited;
-    static Point[] start;
-    static int answer = Integer.MIN_VALUE;
-    static int[] moveRow = {-1, 1, 0, 0};
-    static int[] moveCol = {0, 0, -1, 1};
-    static int N;
-    static int M;
+    private static int answer = Integer.MIN_VALUE;
+    private static int[][] move = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+    private static int N;
+    private static int M;
+    private static int K;
+    private static int[][] map;
+    private static boolean[][] visited;
+    private static int count;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken()); // 세로
+        M = Integer.parseInt(st.nextToken()); // 가로
+        K = Integer.parseInt(st.nextToken()); // 음식물 개수
 
-        map = new boolean[N+1][M+1]; // 음식물이 있으면 true
-        visited = new boolean[N+1][M+1]; // 방문했으면 true
-        start = new Point[K];
-
+        map = new int[N+1][M+1];
+        visited = new boolean[N+1][M+1];
+        ArrayList<Point> points = new ArrayList<>();
         for(int i=0; i<K; i++){
             st = new StringTokenizer(br.readLine());
+
             int r = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
-            map[r][c] = true;
-            start[i] = new Point(r,c);
+            map[r][c] = 1;
+
+            points.add(new Point(r,c));
         }
 
-        for (Point p : start) {
-            if (!visited[p.r][p.c]){
-                int count = BFS(p);
-                if(count >= answer) answer = count;
-            }
+        for(Point p : points){
+            count = 0;
+            dfs(p.r, p.c);
+
+            answer = Math.max(count, answer);
         }
 
         System.out.println(answer);
-
     }
 
-    static private int BFS(Point p){
-        Queue<Point> q = new LinkedList<>();
-        q.add(p);
-        visited[p.r][p.c] = true;
-        int count = 0;
+    private static void dfs(int r, int c){
+        visited[r][c] = true;
+        count ++;
 
-        while(!q.isEmpty()){
-            Point thisPoint = q.poll();
-            count++;
+        for(int i=0; i<4; i++){
+            int nextR = r+move[i][0];
+            int nextC = c+move[i][1];
 
-            for(int i=0; i<4; i++){
-                if(i==0 && thisPoint.r <= 1) continue;
-                if(i==1 && thisPoint.r >= N) continue;
-                if(i==2 && thisPoint.c <= 1) continue;
-                if(i==3 && thisPoint.c >= M) continue;
+            if(nextR < 1 || nextC < 1 || nextR > N || nextC > M) continue;
+            if(visited[nextR][nextC]) continue;
+            if(map[nextR][nextC] != 1) continue;
 
-                Point nextPoint = new Point(thisPoint.r+moveRow[i], thisPoint.c+moveCol[i]);
-                if(!visited[nextPoint.r][nextPoint.c] && map[nextPoint.r][nextPoint.c]){
-                    visited[nextPoint.r][nextPoint.c] = true;
-                    q.add(nextPoint);
-                }
-            }
+            dfs(nextR, nextC);
         }
-        return count;
     }
 }
