08b6e0d2d956af8aa0778e181fca735aa2383cd8
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-10-17 18:26:48 +0900

    [Silver II] Title: 부분수열의 합, Time: 148 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    private static int N;
+    private static int S;
+    private static int[] nums;
+    private static int answer = 0;
+    private static boolean[] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+
+        nums = new int[N];
+        visited = new boolean[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+        for(int j=0; j<N; j++){
+            for(int i=1; i<=N; i++){
+                countSum(i, j, 1, nums[j]);
+            }
+        }
+
+        System.out.println(answer);
+
+    }
+    private static void countSum(int count, int start, int depth, int sum){
+        if(count == depth){
+            if(sum == S) answer ++;
+            return;
+        }
+
+        for(int i=start+1; i<N; i++){
+            countSum(count, i, depth+1, sum+nums[i]);
+        }
+    }
+}
