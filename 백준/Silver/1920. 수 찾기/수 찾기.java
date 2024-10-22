4a979cbba0e0da70b0d5b8d64669a2a9e064b971
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-10-22 11:08:02 +0900

    [Silver IV] Title: 수 찾기, Time: 1136 ms, Memory: 52412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,60 +1,48 @@
-import java.awt.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-
-        int[] nums1 = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++){
-            nums1[i] = Integer.parseInt(st.nextToken());
-        }
 
-        int M = Integer.parseInt(br.readLine());
-        int[] originNums2 = new int[M];
+        int N = Integer.parseInt(st.nextToken());
+
+        long[] nums = new long[N];
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<M; i++){
-            originNums2[i] = Integer.parseInt(st.nextToken());
-        }
+        for(int i=0; i<N; i++) nums[i] = Long.parseLong(st.nextToken());
 
-        int[] nums2 = new int[M];
-        for(int i=0; i<M; i++){
-            nums2[i] = originNums2[i];
-        }
+        st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
 
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
+        long[] targets = new long[M];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<M; i++) targets[i] = Long.parseLong(st.nextToken());
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        int index1 = 0;
-        int index2 = 0;
-
-        while(true){
-            if(index1 >= N) break;
-            if(index2 >= M) break;
-
-            if(nums1[index1] == nums2[index2]){
-                map.put(nums2[index2], 1);
-                index2 ++;
-            }
-            else if(nums1[index1] < nums2[index2]){
-                index1 ++;
-            }
-            else if(nums1[index1] > nums2[index2]){
-                index2 ++;
-            }
-        }
+        // nums 정렬
+        Arrays.sort(nums);
 
         for(int i=0; i<M; i++){
-            if(map.get(originNums2[i]) == null) System.out.println(0);
-            else System.out.println(map.get(originNums2[i]));
+            int low = 0;
+            int high = N-1;
+            int mid;
+
+            boolean hasFound = false;
+
+            while(low <= high){
+                mid = (low+high)/2;
+
+                if(nums[mid] == targets[i]){
+                    hasFound = true;
+                    break;
+                }
+                else if(nums[mid] < targets[i]) low = mid+1;
+                else high = mid-1;
+            }
+
+            if (hasFound) System.out.println(1);
+            else System.out.println(0);
+
         }
     }
 }
