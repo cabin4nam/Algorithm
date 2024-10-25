bbd735006e04b1f4319811ee31ec103b91a8b87a
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-10-25 16:16:37 +0900

    [level 3] Title: 베스트앨범, Time: 1.61 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,62 +1,47 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Collections;
+import java.util.ArrayList;
 class Solution {
     private static class Song{
         int id;
         int play;
-        
         public Song(int id, int play){
             this.id = id;
             this.play = play;
         }
     }
-    
-    public List<Integer> solution(String[] genres, int[] plays) {
-        List<Integer> answer = new ArrayList<>();
+    public ArrayList<Integer> solution(String[] genres, int[] plays) {
+        ArrayList<Integer> answer = new ArrayList<>();
         
-        Map<String, Integer> genreCount = new HashMap<>(); // 장르별 총 재생 횟수
-        Map<String, List<Song>> songCount = new HashMap<>(); // 장르 별 곡의 재생
-        
-        for(int i=0; i<plays.length; i++){
-            String genre = genres[i];
-            genreCount.put(genre, genreCount.getOrDefault(genre, 0) + plays[i]);
+        HashMap<String, ArrayList<Song>> map = new HashMap<>();
+        HashMap<String, Integer> sum = new HashMap<>();
+        for(int i=0; i<genres.length; i++){
+            if(map.get(genres[i]) == null) map.put(genres[i], new ArrayList<>());
             
-            if(!songCount.containsKey(genre)) songCount.put(genre, new ArrayList<>());
-            songCount.get(genre).add(new Song(i, plays[i]));
+            map.get(genres[i]).add(new Song(i, plays[i]));
+            sum.put(genres[i], sum.getOrDefault(genres[i],0) + plays[i]);
         }
         
-        // 정렬
-        List<String> genreKeySet = new ArrayList<>(genreCount.keySet());
-        Collections.sort(genreKeySet, new Comparator<String>(){
-            @Override
-            public int compare(String s1, String s2){
-                return genreCount.get(s2) - genreCount.get(s1);
-            }
-        });
-        
-        for(String key : genreKeySet){
-            List<Song> songs = songCount.get(key);
-            
-            Collections.sort(songs, new Comparator<Song>(){
-               @Override
-                public int compare(Song s1, Song s2){
-                    if(s1.play < s2.play) return 1;
-                    else if(s1.play > s2.play) return -1;
-                    else {
-                        return s1.id - s2.id;
-                    }
+        // genre를 오름차순 정렬 (sum)
+        ArrayList<String> sumKeySet = new ArrayList<>(sum.keySet());
+        Collections.sort(sumKeySet, (s1, s2) -> sum.get(s2) - sum.get(s1));
+
+        // genre별로 노래 오름차순 정렬 (같으면 고유 번호가 낮은 것부터) (map)
+        for (String key : sumKeySet) {
+            Collections.sort(map.get(key), (s1, s2) -> {
+                if (s1.play == s2.play) {
+                    return s1.id - s2.id; // play 횟수가 같으면 id 오름차순 정렬
                 }
+                return s2.play - s1.play; // play 횟수로 오름차순 정렬
             });
-        }
-        
-        for(String key : genreKeySet){
-            List<Song> songs = songCount.get(key);
-            
-            for(int i=0; i<songs.size(); i++){
-                if(i>1) break;
-                answer.add(songs.get(i).id);
+
+            for (int i = 0; i < map.get(key).size(); i++) {
+                if (i >= 2) break;
+
+                answer.add(map.get(key).get(i).id);
             }
         }
-        
+
         
         
         return answer;
