1d3ed9795b86b1fb9e944a0fcf1cb2bb8067b199
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-10-25 16:33:21 +0900

    [level 2] Title: 기능개발, Time: 1.30 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,40 +1,34 @@
 import java.util.*;
-
 class Solution {
     public ArrayList<Integer> solution(int[] progresses, int[] speeds) {
         ArrayList<Integer> answer = new ArrayList<>();
         
-        int[] doneTime = new int[progresses.length];
-        for(int i=0; i<progresses.length; i++){
-            doneTime[i] = calcTime(progresses[i], speeds[i]);
+        Queue<Integer> q = new LinkedList<>();
+        // 7 3 9
+        int taskCnt = progresses.length;
+        for(int i=0; i<taskCnt; i++){
+            int remain = 100-progresses[i];
+            
+            if(remain%speeds[i]==0) q.offer(remain / speeds[i]);
+            else q.offer(remain / speeds[i] + 1);
         }
         
-        Queue<Integer> queue = new LinkedList<Integer>();
-        for(int i=0; i<doneTime.length; i++) queue.offer(doneTime[i]);
-        
-        while(!queue.isEmpty()){
-            int time = queue.poll();
+        while(!q.isEmpty()){
+            int now = q.poll();
             
-            int count = 1;
-            while(!queue.isEmpty() && queue.peek() <= time){
-                queue.poll();
-                count ++;
+            int count =1;
+            while(true){
+                if(q.isEmpty() || q.peek() > now){
+                    break;
+                }
+                
+                q.poll();
+                count++;
             }
             
             answer.add(count);
         }
         
-        
         return answer;
-    }
-    private int calcTime(int progress, int speed){
-        int remain = 100 - progress;
-        int time = 0;
-        
-        time = remain/speed;
-        
-        if(remain%speed > 0) time++;
-        
-        return time;
     }
 }
