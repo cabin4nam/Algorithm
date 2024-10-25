52523651c28474b79948abc87e6703860139fa09
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-10-25 10:13:46 +0900

    [Silver I] Title: 기타리스트, Time: 108 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/1495. 기타리스트/기타리스트.java
+++ 백준/Silver/1495. 기타리스트/기타리스트.java
@@ -1,46 +1,42 @@
 import java.io.*;
-import java.util.StringTokenizer;
-
-public class Main {
-    static int N;
-    static int S;
-    static int M;
-    static int[] volume;
-    static boolean[][] dp;
-    static int max = -1;
-    public static void main(String[] args) throws IOException{
+import java.util.*;
+public class Main{
+    private static int N;
+    private static int S;
+    private static int M;
+    private static int[] gap;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        S = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken()); // 곡의 개수
+        S = Integer.parseInt(st.nextToken()); // 시작 볼륨
+        M = Integer.parseInt(st.nextToken()); // 최대 볼륨
 
-        volume = new int[N+1];
-        dp = new boolean[N+1][M+1];
+        gap = new int[N+1];
         st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) gap[i] = Integer.parseInt(st.nextToken());
+
+        boolean[][] dp = new boolean[N+1][M+1];
+        for(boolean[] d : dp) Arrays.fill(d, false);
+
+        dp[0][S] = true;
         for(int i=1; i<=N; i++){
-            volume[i] = Integer.parseInt(st.nextToken());
-        }
-
-        if(S-volume[1] >= 0) dp[1][S-volume[1]] = true;
-        if(S+volume[1] <= M) dp[1][S+volume[1]] = true;
-
-        for(int i=1; i<N; i++){
-            for(int j=0; j<= M; j++){
-                if(!dp[i][j]) continue;
-
-                if(j-volume[i+1] >= 0) dp[i+1][j-volume[i+1]] = true;
-                if(j+volume[i+1] <= M) dp[i+1][j+volume[i+1]] = true;
+            for(int j=0; j<=M; j++){
+                if(dp[i-1][j]){
+                    if(j + gap[i] <= M) dp[i][j+gap[i]] = true;
+                    if(j - gap[i] >= 0) dp[i][j-gap[i]] = true;
+                }
             }
         }
 
-        for(int i=0; i<=M; i++){
-            if(dp[N][i] && i>max) max = i;
+        for(int i=M; i>=0; i--){
+            if(dp[N][i]) {
+                System.out.println(i);
+                return;
+            }
         }
 
-        System.out.println(max);
-
-
+        System.out.println(-1);
     }
 }
