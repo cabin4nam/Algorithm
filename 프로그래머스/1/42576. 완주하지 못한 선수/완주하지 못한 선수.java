d6607b3656175302a5f68786fbf72eaf43cc5b2f
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-10-25 14:52:06 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 88.40 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,23 +1,18 @@
-import java.util.*;
-
+import java.util.HashMap;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         
-        Map<String, Integer> map = new HashMap<>();
-        for(String p : participant) map.put(p, map.getOrDefault(p, 0)+1);
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String s : participant) map.put(s, map.getOrDefault(s, 0)+1);
         
-        for(String c : completion){
-            if(map.containsKey(c)){
-                 map.put(c, map.get(c)-1);
-            }
-        }
+        for(String s : completion) map.put(s, map.get(s)-1);
         
-        for(String key : map.keySet()){
-            if(map.get(key) > 0){
-                answer = key;
+        for(String s : map.keySet()){
+            if(map.get(s) > 0){
+              answer = s;
                 break;
-            }
+            } 
         }
         
         return answer;
