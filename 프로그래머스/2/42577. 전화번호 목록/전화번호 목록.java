880f8af8ef943ad25b307508a0311d8c760c6747
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-10-25 15:27:38 +0900

    [level 2] Title: 전화번호 목록, Time: 250.71 ms, Memory: 182 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,17 +1,20 @@
-import java.util.*;
+import java.util.HashMap;
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
+        HashMap<String, Integer> map = new HashMap<>();
         
-        Arrays.sort(phone_book);
+        for(int i=0; i<phone_book.length; i++) map.put(phone_book[i], i);
         
-        for(int i=0; i<phone_book.length-1; i++){
-            if(phone_book[i+1].startsWith(phone_book[i])){
-                answer = false;
-                break;
+        for(int i=0; i<phone_book.length; i++){
+            for(int j=1; j<phone_book[i].length(); j++){
+                String sub = phone_book[i].substring(0,j);
+                
+                if(map.containsKey(sub) && i != map.get(sub)){
+                    return false;
+                }
             }
         }
         
-        return answer;
+        return true;
     }
 }
