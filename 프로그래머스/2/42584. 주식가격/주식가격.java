6d7ea7c34c727ca3bc5e48407a240cfbf35231a9
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-10-26 21:42:53 +0900

    [level 2] Title: 주식가격, Time: 54.72 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,23 +1,37 @@
 import java.util.Stack;
 class Solution {
+    private static class Stock{
+        int idx;
+        int price;
+        public Stock(int idx, int price){
+            this.idx = idx;
+            this.price = price;
+        }
+    }
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
         
-        Stack<Integer> stack = new Stack<>();
-        stack.push(0);
+        Stack<Stock> stack = new Stack<>();
+        int now = 0;
         
-        for(int i=1; i<prices.length; i++){
-            while(!stack.empty() && prices[i] < prices[stack.peek()]){
-                int j = stack.pop();
-                answer[j] = i-j;
-            }  
-            stack.push(i);
+        while(now < prices.length){
+            // 다음 주식(prices[idx]) 가격이 더 크거나 같으면 push
+            if(stack.isEmpty() || prices[now] >= stack.peek().price){
+                stack.push(new Stock(now, prices[now]));
+                now++;
+            } 
+            
+            // 다음 주식 가격이 더 작으면 pop, answer갱신
+            else{
+                Stock s = stack.pop();
+                answer[s.idx] = now - s.idx;
+            }
         }
         
-        while(!stack.empty()){
-            int i = stack.pop();
+        while(!stack.isEmpty()){
+            Stock s = stack.pop();
             
-            answer[i] = prices.length-i-1;
+            answer[s.idx] = prices.length-s.idx-1;
         }
         
         return answer;
