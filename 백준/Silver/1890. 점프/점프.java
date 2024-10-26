82741c3494d739075d3891cf15f0e917eefd65f8
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-10-26 21:12:41 +0900

    [Silver I] Title: 점프, Time: 108 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.java
+++ 백준/Silver/1890. 점프/점프.java
@@ -1,35 +1,38 @@
-import java.awt.*;
-import java.util.*;
 import java.io.*;
-
-public class Main {
-    static int N;
-    static int[][] map;
-    static long[][] dp;
+import java.util.*;
+public class Main{
     public static void main(String[] args) throws IOException {
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       N = Integer.parseInt(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-       map = new int[N][N];
-       dp = new long[N][N];
-       for(int i=0; i<N; i++){
-           StringTokenizer st = new StringTokenizer(br.readLine());
-           for(int j=0; j<N; j++){
-               map[i][j] = Integer.parseInt(st.nextToken());
-               dp[i][j] = 0;
-           }
-       }
-       
-        dp[0][0] = 1;
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                if(i==N-1 && j==N-1) continue; //도착 지점
-                int next = map[i][j];
-                if(i+next < N) dp[i+next][j] += dp[i][j]; // 세로이동 (아래로 이동)
-                if(j+next < N) dp[i][j+next] += dp[i][j]; // 가로이동 (오른쪽으로 이동)
+        int n = Integer.parseInt(st.nextToken());
+        int[][] map = new int[n][n];
+
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<n; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        System.out.println(dp[N-1][N-1]);
+
+        long[][] dp = new long[n][n];
+        dp[0][0] = 1;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(map[i][j] == 0) break;
+
+                // 아래쪽으로 가는 경우
+                int down = i+map[i][j];
+                if(down < n) dp[down][j] += dp[i][j];
+
+                // 오른쪽으로 가는 경우
+                int right = j+map[i][j];
+                if(right < n) dp[i][right] += dp[i][j];
+            }
+        }
+
+        System.out.println(dp[n-1][n-1]);
 
     }
 }
