ae5f43885cb26eef5293df33cc8bf5bec40fe83e
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-10-28 12:14:55 +0900

    [level 1] Title: 모의고사, Time: 2.06 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,32 +1,50 @@
 import java.util.*;
-
 class Solution {
-    public int[] solution(int[] answers) {
-        int[] first = {1,2,3,4,5}; // 5개씩 반복
-        int[] second = {2,1,2,3,2,4,2,5}; // 8개씩 반복
-        int[] third = {3,3,1,1,2,2,4,4,5,5}; // 10개씩 반복
-        int[] score = {0,0,0}; // 각 수포자들의 점수
+    public ArrayList<Integer> solution(int[] answers) {
+        ArrayList<Integer> answer = new ArrayList<>();
         
-        // 수포자들의 점수 계산
-        for(int i=0; i<answers.length; i++) {
-            if(answers[i] == first[i%5]) score[0]++;
-            if(answers[i] == second[i%8]) score[1]++;
-            if(answers[i] == third[i%10]) score[2]++;
+        int[] arr1 = new int[answers.length];
+        int[] arr2 = new int[answers.length];
+        int[] arr3 = new int[answers.length];
+        
+        // 수포자 답안 배열 완성
+        for(int i=0; i<answers.length; i++){
+            // 1번 수포자
+            arr1[i] = (i+5)%5+1;
+            
+            // 2번 수포자
+            // 1 3 5 7, 9 11 13 15 17
+            if(i%2 == 0) arr2[i] = 2;
+            else {
+                if(i%8 == 1) arr2[i] = 1;
+                else if(i%8 == 3) arr2[i] = 3;
+                else if(i%8 == 5) arr2[i] = 4;
+                else if(i%8 == 7) arr2[i] = 5;
+            }
+            
+            // 3번 수포자
+            // 0 1 / 2 3 / 4 5 / 6 7 / 8 9 // 10 11 / 12 13 /
+            // 33 11 22 44 55/ 33
+            if(i%10 <= 1) arr3[i] = 3;
+            else if(i%10 <= 3) arr3[i] = 1;
+            else if(i%10 <= 5) arr3[i] = 2;
+            else if(i%10 <= 7) arr3[i] = 4;
+            else if(i%10 <= 9) arr3[i] = 5;
         }
         
-        // 최대 점수 구하기
-        // int max = Math.max(score[0], Math.max(score[1], score[2]));
-        int max = Arrays.stream(score).max().getAsInt();
-        
-        // 최대 점수를 가진 수포자 리스트 생성
-        List<Integer> answ = new ArrayList<Integer>();
-        for(int i=0; i<score.length; i++) if(max == score[i]) answ.add(i+1);
-        
-        int[] answer = new int[answ.size()];
-        for(int i=0; i<answ.size(); i++){
-            answer[i] = answ.get(i);
+        int cnt1=0, cnt2 =0, cnt3 =0;
+        for(int i=0; i<answers.length; i++){
+            if(arr1[i] == answers[i]) cnt1++;
+            if(arr2[i] == answers[i]) cnt2++;
+            if(arr3[i] == answers[i]) cnt3++;
         }
-
+        
+        int max = Math.max(cnt1, Math.max(cnt2, cnt3));
+        
+        if(cnt1 == max) answer.add(1);
+        if(cnt2 == max) answer.add(2);
+        if(cnt3 == max) answer.add(3);
+        
         return answer;
     }
 }
