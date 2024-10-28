347cf056d98005b6784f555a5de6367a7b1cd72c
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-10-28 11:06:36 +0900

    [level 2] Title: 가장 큰 수, Time: 192.77 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,56 +1,24 @@
-// 완전탐색 -> 시간 초과
-// class Solution {
-//     private static boolean[] visited;
-//     private static String answer;
-//     public String solution(int[] numbers) {
-//         answer = "";
-        
-//         visited = new boolean[numbers.length];
-//         dfs(numbers, 0, "");
-        
-//         return answer;
-//     }
-    
-//     public void dfs(int[] numbers, int depth, String result){
-//         if(depth == numbers.length){
-//             if(answer.equals("")) answer = result; 
-//             else if(Integer.parseInt(answer) < Integer.parseInt(result)){
-//                 answer = result;
-//             }
-//             return;
-//         }
-        
-//         for(int i=0; i<numbers.length; i++){
-//             if(!visited[i]){
-//                 visited[i] = true;
-//                 dfs(numbers, depth +1, result+numbers[i]);
-//                 visited[i] = false;
-//             }
-//         }
-//     }
-// }
-
 import java.util.*;
 class Solution {
+    private static ArrayList<String> list;
+    private static int n;
+    private static boolean[] visited;
     public String solution(int[] numbers) {
         String answer = "";
         
-        List<String> numList = new ArrayList<>(numbers.length);
-        for(int n : numbers){
-            numList.add(Integer.toString(n));
-        }
+        String[] strs = new String[numbers.length];
+        for(int i=0; i<numbers.length; i++) strs[i] = String.valueOf(numbers[i]);
         
-        Collections.sort(numList, new Comparator<String>(){
-            @Override
-            public int compare(String s1, String s2){
-                return (s2+s1).compareTo(s1+s2);
-            }
-        });
+        Arrays.sort(strs, (n1, n2) -> (n2+n1).compareTo(n1+n2));
         
-        for(String n : numList){
-            answer += n;
-        }
-        if(numList.get(0).equals("0")) answer = "0";
+        if(strs[0].equals("0")) return "0";
+        
+        StringBuilder sb = new StringBuilder();
+        for(String s : strs) sb.append(s);
+        
+        answer = sb.toString();
+        
         return answer;
     }
+    
 }
