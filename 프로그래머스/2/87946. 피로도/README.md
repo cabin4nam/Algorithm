e1ea573c83dbe68c48ecb65ee9afa1f966ce1989
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-10-29 09:40:53 +0900

    [level 2] Title: 피로도, Time: 4.65 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 67.2 MB, 시간: 0.04 ms
+메모리: 71.6 MB, 시간: 4.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 6일 19:7:48
+2024년 10월 29일 09:40:50
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,36 +1,26 @@
 class Solution {
-    static boolean[] visited;
-    static int answer = -1;
+    private static int answer;
     public int solution(int k, int[][] dungeons) {
-        visited = new boolean[dungeons.length];
+        answer = -1;
         
-        for(int i=0; i<dungeons.length; i++){
-            visited[i] = true;
-            adventure(k-dungeons[i][1], dungeons, 1);
-            visited[i] = false;
-        }
+        boolean[] visited = new boolean[dungeons.length];
+        
+        goDungeon(k, dungeons, visited, 0);
         
         return answer;
     }
     
-    private void adventure(int px, int[][] dungeons, int count){
-        boolean canGo = false;
+    public void goDungeon(int k, int[][] dungeons, boolean[] visited, int count){
+        answer = Math.max(answer, count);
         
         for(int i=0; i<dungeons.length; i++){
-            if(visited[i]) continue;
-            
-            if(px >= dungeons[i][0]){
-                canGo = true;
-                visited[i] = true;
-                adventure(px-dungeons[i][1], dungeons, count+1);
-                visited[i] = false;
-            } 
+            if(!visited[i]){
+                if(k >= dungeons[i][0]){
+                    visited[i] = true;
+                    goDungeon(k-dungeons[i][1], dungeons, visited, count+1);
+                    visited[i] = false;
+                }
+            }
         }
-        
-        if(!canGo){
-            if(answer < count) answer = count;
-            return;
-        } 
-        
     }
 }
