9e4c3adb55218d33c79f47f40ebf13b6ec9a129b
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-10-30 12:45:15 +0900

    [level 2] Title: 구명보트, Time: 11.20 ms, Memory: 55.1 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
 
 ### 성능 요약
 
-메모리: 53.4 MB, 시간: 7.97 ms
+메모리: 55.1 MB, 시간: 11.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 3일 20:57:1
+2024년 10월 30일 12:45:14
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,38 +1,23 @@
-import java.util.*;
+import java.util.Arrays;
+
 class Solution {
-    static int cnt=0;
     public int solution(int[] people, int limit) {
         int answer = 0;
-
+        
+        // 50 50 70 80
         Arrays.sort(people);
-
-        int start = 0;
-        int end = people.length - 1;
-
-        while (start <= end) {
-            if (people[start] + people[end] <= limit) {
-                start++;
+        
+        int left = 0;
+        int right = people.length-1;
+        while (left <= right) {
+            answer++; // 보트 사용
+            if (people[left] + people[right] <= limit) {
+                left++; // 가벼운 사람 태움
             }
-            end--;
-            answer++;
+            right--; // 무거운 사람 태움
         }
-
+        
         return answer;
-    }
     
-    public void func(int start, int end, int weightSum, int limit, int[] people){
-        if(start >= people.length || end >= people.length){
-            cnt++;
-            return;
-        }
-        
-        if(weightSum + people[end] <= limit){
-            func(start, end+1, weightSum+people[end], limit, people);
-        } 
-        else {
-            cnt++;
-            func(end, end+1, people[end], limit, people);
-        }
-        
     }
 }
