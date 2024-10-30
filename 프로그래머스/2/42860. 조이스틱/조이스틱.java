0b9d25f3b39b8dadd776d67302b806312bffabde
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-10-30 11:38:48 +0900

    [level 2] Title: 조이스틱, Time: 0.11 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,24 +1,26 @@
-import java.util.*;
-
 class Solution {
     public int solution(String name) {
         int answer = 0;
-        int index;
-        int move = name.length()-1; // 좌우 조작 1번 경우
-        
-        for(int i=0;i<name.length();i++) {
-            // A에서부터 올라가는 게 빠를 지, Z에서부터 내려오는 게 빠를지
-            answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
-            
-            // 다음 인덱스 설정
-            index = i+1;
-            // 만약 다음 인덱스가 A이면, A가 끝나는 지점을 찾아서 index를 이동시키기
-            while(index<name.length() && name.charAt(index) == 'A') {
+        int length = name.length();
+
+        int index; // 다음 값들을 확인할 때 사용
+        int move = length - 1; // 좌우 움직임 수를 체크
+
+        for(int i = 0; i < name.length(); i++){
+            // 다음 알파벳 방향으로 가는 것, 이전 알파벳 방향으로 가는 것
+            answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1); 
+
+            index = i + 1;
+            // 연속되는 A 갯수 확인
+            while(index < length && name.charAt(index) == 'A'){
                 index++;
             }
-            
-            move = Math.min(move, i*2+name.length()-index); // 좌우 조작 2번 경우
-            move = Math.min(move, (name.length()-index)*2 + i); // 좌우 조작 3번 경우
+
+            // 순서대로 가는 것과, 뒤로 돌아가는 것 중 이동수가 적은 것을 선택
+            move = Math.min(move, i * 2 + length - index);
+            // 2022년 이전 테스트 케이스만 확인하면 여기까지해도 정답처리가 되기 때문에, 이전 정답들에는 여기까지만 정리되어 있지만,
+            // BBBBAAAAAAAB 와 같이, 처음부터 뒷부분을 먼저 입력하는 것이 더 빠른 경우까지 고려하려면 아래의 코드가 필요합니다.
+            move = Math.min(move, (length - index) * 2 + i);
         }
         return answer + move;
     }
