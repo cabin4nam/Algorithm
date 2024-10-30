0b57833ce5a3c1fcfeaa14b56263f9efabc20ba4
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-10-30 12:31:19 +0900

    [level 2] Title: 큰 수 만들기, Time: 7956.16 ms, Memory: 99.3 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,38 +1,24 @@
+// 참고 :  https://hyojun.tistory.com/entry/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%ED%81%B0-%EC%88%98-%EB%A7%8C%EB%93%A4%EA%B8%B0-Java
 class Solution {
-    int[] numberArr;
-    public String solution(String number, int k) {
-        String answer = "";
+	public String solution(String number, int k) {
+		String answer = "";
         
-        int remain_cnt = number.length() - k;
-        numberArr = new int[number.length()];
-        for(int i=0; i<number.length(); i++){
-            numberArr[i] = Integer.parseInt(number.substring(i, i+1));
-        }
+		int idx = 0;
+		StringBuilder sb = new StringBuilder();
+		
+		// 앞에서 부터 (전체길이 - k) 수 중 가장 큰 수 선택
+		// 선택한 수 다음 인덱스 부터 탐색 범위를 한칸씩 늘려가며 반복 탐색
+		for(int i = 0; i < number.length() - k; i++) {
+			char max = 0;
+			for(int j = idx; j <= i + k; j++) {
+				if(max < number.charAt(j)) {
+					max = number.charAt(j);
+					idx = j + 1;
+				}
+			}
+			sb.append(max);
+		}
         
-        int start = 0;
-        int end = number.length() - remain_cnt;
-        while(true){
-            if(remain_cnt <= 0) break;
-            
-            int max_index = findMax(start, end);
-            answer += numberArr[max_index];
-            
-            remain_cnt --;
-            start = max_index+1;
-            end = number.length() - remain_cnt;
-            
-        }
-        
-        return answer;
+        return sb.toString();
     }
-    
-    public int findMax(int start, int end){
-        int max_index = start;
-        for(int i=start; i<=end; i++){
-            if(numberArr[max_index] < numberArr[i]) max_index = i;
-        }
-        
-        return max_index;
-    }
-    
 }
