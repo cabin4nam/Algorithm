710e36b2540ad40ecd18bc132307407ef6230d36
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-10-31 10:07:04 +0900

    [level 3] Title: 정수 삼각형, Time: 12.09 ms, Memory: 61.2 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,25 +1,31 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[][] triangle) {
         int answer = 0;
         
-      for(int i = 1; i < triangle.length ; i++) {
-            for(int j = 0; j < triangle[i].length ; j++) {
-
-                if(j == 0) {//왼쪽 끝
-                    triangle[i][j] += triangle[i-1][j];
-                }
-                else if(j == i) {//오른쪽 끝
-                    triangle[i][j] += triangle[i-1][j-1];
-                }
-                else {
-                    triangle[i][j] += Math.max(triangle[i-1][j], triangle[i-1][j-1]);
-                }
-
-                answer = Math.max(answer, triangle[i][j]);
+        int size = triangle.length;
+        
+        int[][] dp = new int[size+1][size+1];
+        dp[0][0] = triangle[0][0];
+        
+        if(size == 1) return triangle[0][0];
+        
+        for(int i=1; i<size; i++){
+            // 이전 층의 수에 이번 층의 수를 더한
+            for(int j=0; j<=i; j++){
+                // 만약 맨 왼쪽 요소라면
+                if(j==0) dp[i][j] = dp[i-1][j] + triangle[i][j];
+                
+                // 만약 맨 오른쪽 요소라면
+                else if(j==i) dp[i][j] = dp[i-1][j-1] + triangle[i][j];
+                
+                else dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j];
             }
         }
-    return answer;
+        
+        for(int n : dp[size-1]){
+            answer = Math.max(answer, n);
+        }
+        
+        return answer;
     }
 }
