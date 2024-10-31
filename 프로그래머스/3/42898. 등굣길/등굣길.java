64abc7d8a675324753e919e5ba8b138e7f9e92dd
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-10-31 10:26:36 +0900

    [level 3] Title: 등굣길, Time: 1.21 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,31 +1,29 @@
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        // 행열을 반대로 알려주고 있음 -> m x n 배열 -> int[n+1][m+1]
-        // 배열 상에서는 int[][]의 각 요소가 세로, 가로 순
-        // 문제 상에서는 int[][]의 각 요소가 가로, 세로 순
-        
-        int mod = 1000000007;
-        
-        int[][] map = new int[n+1][m+1];
-        
-        for(int i=0; i<puddles.length; i++){
-            map[puddles[i][1]][puddles[i][0]] = -1;
+        // m이 가로, n이 세로 길이
+        // dp[n][m] = (m,n)까지 갈 수 있는 최단 경로의 개수
+        int[][] dp = new int[n+1][m+1];
+        dp[1][1] = 1;
+        for(int[] p : puddles){
+            dp[p[1]][p[0]] = -1;
         }
         
-        map[1][1] = 1;
-        for(int i=1; i<n+1; i++){
-            for(int j=1; j<m+1; j++){
-                // 현재 위치가 웅덩이라면 계산하지 않음
-                if(map[i][j] == -1) continue; 
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(dp[i][j] == -1) continue;
                 
-                // 위쪽으로부터 오는 경우 -> 위쪽이 물웅덩이가 아니면 현재 위치로 올 수 있는 경로로 더해줌
-                if(map[i-1][j] > 0) map[i][j] += map[i-1][j] % mod;
+                // 오른쪽으로 이동
+                if(j<m && dp[i][j+1] != -1){
+                    dp[i][j+1] = (dp[i][j+1] + dp[i][j])%1000000007;
+                }
                 
-                // 왼쪽으로부터 오는 경우 -> 위쪽이 물웅덩이가 아니면 현재 위치로 올 수 있는 경로로 더해줌
-                if(map[i][j-1] > 0) map[i][j] += map[i][j-1] % mod;
-             }
+                // 아래쪽으로 이동
+                if(i<n && dp[i+1][j] != -1){
+                    dp[i+1][j] = (dp[i+1][j] + dp[i][j])%1000000007;
+                }
+            }
         }
         
-        return map[n][m]%mod;
+        return dp[n][m];
     }
 }
