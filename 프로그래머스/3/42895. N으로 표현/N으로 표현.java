501b8066ee641bfad6917886e6808707e09d6e19
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-10-31 09:47:39 +0900

    [level 3] Title: N으로 표현, Time: 36.18 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,50 +1,52 @@
-import java.util.*;
+import java.util.HashSet;
 class Solution {
+    private static int SIZE = 32001;
     public int solution(int N, int number) {
         int answer = 0;
         
-        // index는 사용한 N의 개수
-        // ex. List.get(1) -> N을 1개 사용해서 나올 수 있는 숫자의 모든 경우의 수
-        // 중복된 값을 삽입하지 않도록 하기 위해 Set 사용
-        List<Set<Integer>> setList = new ArrayList<>();
+        // number를 N을 사용해서 표현
+        // N을 사용하여 만들 수 있는 것들을 저장, number를 만나면 answer로 지정 후 빠져나가기
+        // dp의 index : 사용한 N의 개수
+        HashSet<Integer>[] dp = new HashSet[SIZE];
         
-        // 8개 이상의 N을 쓰면 -1 반환이므로 그 전까지 수행
-        for(int i=0; i<9; i++){
-            setList.add(new HashSet<>());
-        }
-        
-        setList.get(1).add(N); // N 1개로 만들 수 있는 것은 N 자신 뿐4
-        
-        if(number == N) return 1; // 1개로 만들 수 있는 값이라면 바로 반환
-        
-        // 내가 항상 틀렸던 부분 : 괄호가 있는 경우 ex. (5,26) -> 4 ((5/5) + (5*5))
-        for(int i=2; i<9; i++){
-            for(int j=1; j<=i/2; j++){
-                unionSet(setList.get(i), setList.get(i-j), setList.get(j));
-                unionSet(setList.get(i), setList.get(j), setList.get(i-j));
-            }
-            // 연속된 숫자 넣기 -> 55, 555, 5555
-            String n = Integer.toString(N);
-            setList.get(i).add(Integer.parseInt(n.repeat(i)));
-            for(int num : setList.get(i)){
-                if(num==number) return i;
-            }
-        }
+        dp[1] = new HashSet<>();
+        dp[1].add(N);
+        if(N==number) return 1;
+
+        for(int index =2; index<9; index++){
+            dp[index] = new HashSet<>();
+            // N inedex개 합친 수
+            int conti = 0;
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<index; i++)
+                sb.append(String.valueOf(N));
+            dp[index].add(Integer.parseInt(sb.toString()));
             
+            for(int i=1; i<index; i++){
+                for(int n1 : dp[i]){
+                    for(int n2 : dp[index-i]){
+                        // 더하기
+                        dp[index].add(n1+n2);
+                        
+                        // 빼기
+                        dp[index].add(n1-n2);
+                        
+                        // 곱하기
+                        dp[index].add(n1*n2);
+                        
+                        // 나누기
+                        if(n2 != 0) dp[index].add(n1/n2);
+                    }
+                }
+            }
+            
+            for(int n : dp[index]){
+                if(n==number){
+                    return index;
+                } 
+            } 
+        }
         
         return -1;
-    }
-    
-    // 두 Set을 합치기
-    public void unionSet(Set<Integer> union, Set<Integer> a, Set<Integer> b){
-        for(int n1 : a){
-            for(int n2 : b){
-                // n1과 n2로 만들 수 있는 모든 수의 경우를 삽입
-                union.add(n1+n2); 
-                union.add(n1-n2);
-                union.add(n1*n2);
-                if(n2!=0) union.add(n1/n2);
-            }
-        }
     }
 }
--- 프로그래머스/3/42895. N으로 표현/README.md
+++ 프로그래머스/3/42895. N으로 표현/README.md
@@ -1,10 +1,10 @@
 # [level 3] N으로 표현 - 42895 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895) 
 
 ### 성능 요약
 
-메모리: 97.9 MB, 시간: 37.43 ms
+메모리: 79.3 MB, 시간: 36.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 03일 23:09:19
+2024년 10월 31일 09:47:37
 
 ### 문제 설명
 
