c8283b20f23f3637b09929760b5c41ab24e175db
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-10-31 17:09:01 +0900

    [level 3] Title: 여행경로, Time: 108.17 ms, Memory: 98.8 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/README.md
+++ 프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
 
 ### 성능 요약
 
-메모리: 82 MB, 시간: 13.72 ms
+메모리: 98.8 MB, 시간: 108.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 6일 17:56:32
+2024년 10월 31일 17:08:59
 
 ### 문제 설명
 
--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,52 +1,31 @@
 import java.util.*;
 class Solution {
-    static boolean[] visited;
-    static boolean success = false;
-    static String[] answerArr;
+    private static ArrayList<String> routes;
+    private static boolean[] visited;
     public String[] solution(String[][] tickets) {
-        String[] answer = {};
         
-        // 여행지를 알파벳 순으로 정렬
-        Arrays.sort(tickets, (t1, t2) -> {
-            if(t1[0].equals(t2[0])){
-                return t1[1].compareTo(t2[1]);
-            }
-            else {
-                return t1[0].compareTo(t2[0]);
-            }
-        });
+        routes = new ArrayList<>();
+        visited=  new boolean[tickets.length+1];
+        dfs(0, "ICN","ICN", tickets);
         
-        for(int i=0; i<tickets.length; i++){
-            System.out.println(success);
-            if(success) break;
-            
-            if(tickets[i][0].equals("ICN")){
-                // answerArr = new ArrayList<>();
-                visited = new boolean[tickets.length + 1];
-                // answerArr2 = new ArrayList<>();
-                dfs(i, tickets, "ICN ");
-            }
-        }
-        return answerArr;
+        Collections.sort(routes);
+        
+        return routes.get(0).split(",");
     }
     
-    public void dfs(int start, String[][] tickets, String path){   
-        if(success) return;
-        if(path.split(" ").length==tickets.length){
-            path += tickets[start][1];
-            answerArr =  path.split(" ");
-            success = true;
-            return;  
-        } 
+    private static void dfs(int depth, String now, String route, String[][] tickets){
+        if(depth == tickets.length){
+            routes.add(route);
+            return;
+        }
         
         for(int i=0; i<tickets.length; i++){
-            if(tickets[start][1].equals(tickets[i][0])){
-                if(visited[i]) continue;
-                
-                visited[start] = true;
-                dfs(i, tickets, path + tickets[start][1] + " ");
-                visited[start] = false;
+            if(!visited[i] && now.equals(tickets[i][0])){
+                visited[i] = true;
+                dfs(depth +1, tickets[i][1], route+","+tickets[i][1], tickets);
+                visited[i] = false;
             }
         }
     }
+    
 }
