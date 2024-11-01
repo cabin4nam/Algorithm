91b6fa4f29e36861263a6252fa471d5706183332
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-11-01 13:14:11 +0900

    [level 2] Title: 괄호 회전하기, Time: 65.10 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,37 +1,40 @@
 import java.util.*;
-import java.lang.*;
 class Solution {
-    private static Stack<String> stack = new Stack<>();
     public int solution(String s) {
         int answer = 0;
         
-        HashMap<String, String> map = new HashMap<>();
-        map.put(")", "(");
-        map.put("}", "{");
-        map.put("]", "[");
-        
-        int n = s.length();
-        s += s; // 문자열 뒤에 문자열 한 번 더 붙여서 두 번 반복되게
-        // 회전시키기 -> 시작 인덱스를 하나씩 미뤄줌
-        A : for(int i=0; i<n; i++){
-            stack.clear();
+        Queue<Character> q = new LinkedList<>();
+        for(int i =0; i<s.length(); i++) q.offer(s.charAt(i));
+        for(int i=0; i<s.length(); i++){
+            // 올바른지 확인 -> 올바르면 answer에 더해주기
+            ArrayList<Character> list = new ArrayList<>();
+            for(Character c : q){
+                list.add(c);
+            }   
+            if(isCorrect(list)) answer++;
             
-            for(int j=i; j<i+n; j++){
-                String ch = s.substring(j, j+1);
-                if(!map.containsKey(ch)) // map에 key가 없다면, 열린괄호
-                    stack.push(ch);
-                
-                else {
-                    if(stack.empty() || !(stack.pop().equals(map.get(ch))))
-                        continue A;
-                }
-            }
-            
-            if(stack.isEmpty()) answer ++;
-            
+            // 회전
+            q.offer(q.poll());
         }
-        
-        
         return answer;
+    }
+    
+    private static boolean isCorrect(ArrayList<Character> list){
+        Stack<Character> stack = new Stack<>();
+        
+        for(Character c : list){
+            if(c=='[' || c=='{' || c=='(') stack.push(c);
+            else {
+                if(stack.isEmpty()) return false;
+                
+                Character peek = stack.peek();
+                if(c==']' && peek=='[') stack.pop();
+                else if(c=='}' && peek=='{') stack.pop();
+                else if(c==')' && peek=='(') stack.pop();
+                else return false;
+            }
+        }
+        if(!stack.isEmpty()) return false;
+        return true;
     }
 }
