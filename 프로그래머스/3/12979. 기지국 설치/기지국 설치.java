1dc9ecac44c8ca30ca14dd8623d61417476c405f
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-11-01 13:53:22 +0900

    [level 3] Title: 기지국 설치, Time: 0.96 ms, Memory: 63.2 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,29 +1,27 @@
 class Solution {
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
+
+        int begin = 1;
+        for(int i=0; i<stations.length; i++){
+            if(begin < stations[i]-w){
+                answer += searchCount(begin, stations[i]-w-1, w);
+            }
+            begin = stations[i]+w+1;
+        }
         
-        int unit = 2*w + 1;
-        for(int i=0; i<=stations.length; i++){
-            int start = 1;
-            int end = 1;
-            
-            if(i==stations.length){
-                start = stations[i-1]+w+1;
-                end = n;
-            }
-            
-            else {
-                if(i > 0) start = stations[i-1] + w +1;
-                
-                end = stations[i]-w-1;
-            }
-            
-            
-            int gap = end-start+1;
-            answer += gap/unit;
-            if(gap%unit > 0) answer ++;
+        if(begin <= n){
+            answer += searchCount(begin, n, w);
         }
 
         return answer;
+    }
+    
+    private static int searchCount(int begin, int end, int w){
+        int count = 0;
+        count = (end-begin+1)/(2*w+1);
+        if((end-begin+1)%(2*w+1)!=0) count ++;
+        
+        return count;
     }
 }
