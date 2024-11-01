585fe15968ca3a6bc7abe3f04a026c1765131798
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-11-01 14:42:23 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.73 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,45 +1,49 @@
 import java.util.*;
-
 class Solution {
-    private static class Job {
+    private static class Job{
         int start;
         int time;
-        
-        public Job(int start, int time) {
+        public Job(int start, int time){
             this.start = start;
             this.time = time;
         }
     }
-    
+    private static int count = 0;
     public int solution(int[][] jobs) {
         int answer = 0;
+        
+        // jobs를 시작 시간 기준 정렬
+        Arrays.sort(jobs, (j1, j2) -> j1[0]-j2[0]);
+        int jobsIdx = 0; // jobs 배열의 인덱스
         int time = 0;
-        int idx = 0;
-        int jobCount = jobs.length;
-
-        // 요청 순으로 정렬
-        Arrays.sort(jobs, (j1, j2) -> j1[0] - j2[0]);
-
-        PriorityQueue<Job> pq = new PriorityQueue<>(Comparator.comparingInt(j -> j.time));
-
-        while (idx < jobCount || !pq.isEmpty()) {
-            // 현재 시점까지 요청된 작업을 모두 큐에 추가
-            while (idx < jobCount && jobs[idx][0] <= time) {
-                pq.offer(new Job(jobs[idx][0], jobs[idx][1]));
-                idx++;
+        PriorityQueue<Job> pq = new PriorityQueue<>(new Comparator<Job>(){
+            @Override
+            public int compare(Job j1, Job j2){
+                if(j1.time == j2.time) return j1.start-j2.start;
+                else return j1.time-j2.time;
+            }
+        });
+        
+        while(count < jobs.length){
+            if(pq.isEmpty()){
+                time = jobs[jobsIdx][0];
+            }
+            else{
+                Job j = pq.poll(); // 작업 실행
+                count ++;
+                time += j.time;
+                answer += time-j.start;
             }
             
-            // 작업 가능 상태면 진행
-            if (!pq.isEmpty()) {
-                Job job = pq.poll();
-                time += job.time; // 작업 소요 시간 추가
-                answer += time - job.start; // 요청부터 종료까지 걸린 시간 계산
-            } else {
-                // 처리할 작업이 없으면 시간 이동
-                time = jobs[idx][0];
-            }
+            // j가 끝나기 전에 시작되는 작업들 넣기
+            while (jobsIdx < jobs.length && jobs[jobsIdx][0] <= time) {
+				pq.add(new Job(jobs[jobsIdx][0], jobs[jobsIdx][1]));
+                jobsIdx++;
+			}
+            
         }
         
-        return answer / jobCount;
+        
+        return answer/jobs.length;
     }
 }
