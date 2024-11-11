7f79487beda382875826ccb7d1cff6cbf037aee3
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-11-11 12:05:44 +0900

    [level 1] Title: 시저 암호, Time: 1.71 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,28 +1,19 @@
 class Solution {
     public String solution(String s, int n) {
-        String answer = s;
-        
-        for(int t = 0; t<n; t++){
-            StringBuilder sb = new StringBuilder();
-            for(int i=0; i<answer.length(); i++){
-                char c = answer.charAt(i);
+        StringBuilder answer = new StringBuilder();
 
-                if(c==' '){ // 공백
-                    sb.append(' ');
-                    continue;
-                } 
-                
-                if(!Character.isAlphabetic((char)(c+1))){
-                    if(Character.isUpperCase(c)) sb.append('A');
-                    else if(Character.isLowerCase(c)) sb.append('a');
-                }
-                else sb.append((char)(c+1));
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
 
+            if (c == ' ') { // 공백 처리
+                answer.append(' ');
+            } else if (Character.isUpperCase(c)) { // 대문자 처리
+                answer.append((char) ((c - 'A' + n) % 26 + 'A'));
+            } else if (Character.isLowerCase(c)) { // 소문자 처리
+                answer.append((char) ((c - 'a' + n) % 26 + 'a'));
             }
-            answer = sb.toString();
         }
-        
-        
+
         return answer.toString();
     }
 }
