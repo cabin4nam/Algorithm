be87ab0c87f244552b792f850bcea2fc4ff9fa6b
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-11-14 16:19:45 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 19.72 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,51 +1,54 @@
 import java.util.*;
 
 class Solution {
-    // 만들 수 있는 메뉴 구성과 총 주문 수를 저장할 해시맵
-    private static HashMap<Integer, HashMap<String, Integer>> courseMap;
-    
-    public String[] solution(String[] orders, int[] course) {
-        // 해시맵 초기화
-        courseMap = new HashMap<>();
-        for(int i : course){
-            courseMap.put(i, new HashMap<>());
-        }
-        
-        // 코스를 배열로 만들고, 오름차순 정렬해서, 가능한 모든 메뉴 구성 구하기
-        for(String order : orders){
-            char[] orderArray = order.toCharArray();
-            Arrays.sort(orderArray); // 각 String 내의 character를 정렬, String 간의 정렬은 일어나지 않음
-            combinations(0, orderArray, "");
-        }
-        
+    public ArrayList<String> solution(String[] orders, int[] course) {
         ArrayList<String> answer = new ArrayList<>();
         
-        // 모든 코스 후보에 대해서
-        for(HashMap<String, Integer> count : courseMap.values()){
-            count.values().stream()
-                .max(Comparator.comparingInt(o -> o)) // 가장 빈도 수가 높은 코스를 찾음
-                .ifPresent(cnt -> count.entrySet().stream()
-                           // 최소 2명 이상의 손님으로부터 주문된 단품 메뉴 조합에 대해서만
-                           .filter(entry -> cnt.equals(entry.getValue()) && cnt > 1)
-                           //코스 메뉴를 answer 리스트에 추가
-                           .forEach(entry -> answer.add(entry.getKey())));
+        for(int c : course){
+            HashMap<String, Integer> map = new HashMap<>();
+            
+            for(String o : orders){
+                boolean[] visited = new boolean[o.length()];
+                char[] charArr = o.toCharArray();
+                Arrays.sort(charArr);
+                for(int i=0; i<charArr.length; i++){
+                    visited[i] = true;
+                    makeMenu(i, c, String.valueOf(charArr), map, visited, String.valueOf(charArr[i]));
+                    visited[i] = false;
+                }
+            }
+            
+            // map을 value 기준 내림차순 정렬
+            ArrayList<String> keys = new ArrayList<>(map.keySet());
+            Collections.sort(keys, (k1, k2) -> map.get(k2)-map.get(k1));
+            
+            int max = map.get(keys.get(0));
+            if(max < 2) break;
+            
+            for(String k : keys){
+                if(max == map.get(k)) answer.add(k);
+                else break;
+            }
+            
         }
         
         Collections.sort(answer);
-        return answer.toArray(new String[0]);
         
+        return answer;
     }
     
-    // 만들 수 있는 모든 조합을 재귀 함수를 이용해서 구현
-    public static void combinations(int idx, char[] order, String result){
-        // 필요한 코스 메뉴의 수와 일치하는 것만 저장
-        if(courseMap.containsKey(result.length())){
-            HashMap<String, Integer> map = courseMap.get(result.length());
-            map.put(result, map.getOrDefault(result, 0) + 1);
+    private static void makeMenu(int start, int count, String order, HashMap<String, Integer> map, boolean[] visited, String result){
+        if(result.length() == count){
+            map.put(result, map.getOrDefault(result, 0)+1);
+            return;
         }
         
-        for(int i=idx; i<order.length; i++){
-            combinations(i+1, order, result+order[i]);
+        for(int i=start+1; i<order.length(); i++){
+            if(!visited[i]){
+                visited[i] = true;
+                makeMenu(i, count, order, map, visited, result+order.substring(i,i+1));
+                visited[i] = false;
+            }
         }
     }
 }
