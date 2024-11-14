745ab9299f4b8c2736d42dd8954252d7221bff1a
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-11-14 14:51:00 +0900

    [level 3] Title: 불량 사용자, Time: 188.45 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -1,76 +1,53 @@
 import java.util.*;
-
 class Solution {
-    private static ArrayList<Integer>[] matched;
     private static boolean[] visited;
-    private static int size;
-    private static int answer = 0;
+    private static int answer;
     private static HashSet<String> set;
     public int solution(String[] user_id, String[] banned_id) {
-        visited = new boolean[user_id.length];
+        answer = 0;
         
-        size = banned_id.length;
-        matched = new ArrayList[size];
-        
-        // 각 banned_id에 매칭될 수 있는 user_id를 저장해주기
-        for(int i=0; i<size; i++){
-            matched[i] = findMatched(user_id, banned_id[i]);
-        }
-        
-        // 중복을 허용하지 않는 자료구조에 user_id조합을 저장해, 개수 세기
         set = new HashSet<>();
-        dfs(user_id, 0, "");
-        
+        visited = new boolean[user_id.length];
+        dfs(user_id, banned_id, 0, "");
+
         answer = set.size();
-        
         return answer;
     }
     
-    public void dfs(String[] user_id, int idx, String res){
-        // 모든 banned_id를 탐색 완료하면
-        if(idx >= size){
-            String[] str = res.split(" ");
-            Arrays.sort(str);            
-            StringBuilder sb = new StringBuilder();
-            for(String s : str) sb.append(s);
-            set.add(sb.toString());
+    private static void dfs(String[] user_id, String[] banned_id, int index, String result){
+        if(index == banned_id.length){ // 끝까지 탐색 성공했으면
+            String[] ids = result.split(" ");
+            Arrays.sort(ids);
+            
+            StringBuilder str=new StringBuilder();
+			for(String s:ids) str.append(s);//정렬된 id들을 하나의 문자열로 추가
+			set.add(str.toString());
+            
             return;
         }
         
-        // 현재 idx와 매칭될 수 있는 문자열 중, 아직 방문하지 않은 것에 대해 추가해주기
-        for(int i=0; i<matched[idx].size(); i++){
-            int selected = matched[idx].get(i);
-            
-            if(!visited[selected]){
-                visited[selected] = true;
-                dfs(user_id, idx+1, res+ " " + user_id[selected]);
-                visited[selected] = false;
-            }
+        String target = banned_id[index]; // 불량사용자 아이디 패턴에 맞는 사용자 찾기
+        
+        for(int i=0; i<user_id.length; i++){
+            // target에 맞는 사용자 아이디라면
+            if(isMatched(target, user_id[i]) && !visited[i]){
+                // visited = true 처리한 후, 깊이 우선 탐색 
+                visited[i] = true;
+                dfs(user_id, banned_id, index+1, result+" " +user_id[i]);
+                visited[i] = false;
+            } 
         }
     }
     
-    // banned사용자 문자열과 맞는 user id를 저장해주기
-    public ArrayList<Integer> findMatched(String[] user_id, String target){
-        ArrayList<Integer> matched = new ArrayList<>();
-        for(int i = 0; i<user_id.length; i++){
-            boolean isMatched = true;
-            if(user_id[i].length() != target.length() || visited[i]){
-                isMatched = false;
+    private static boolean isMatched(String target, String id){
+        if(target.length() != id.length()) return false;
+        
+        for(int i=0; i<target.length(); i++){
+            if(target.charAt(i)!='*' && target.charAt(i) != id.charAt(i)){
+                return false;
             }
-            
-            else {
-                for(int j=0; j<user_id[i].length(); j++){
-                    if(target.charAt(j)!='*' && user_id[i].charAt(j) != target.charAt(j)){
-                        isMatched = false;
-                        break;
-
-                    }
-                }
-            }
-            
-            if(isMatched) matched.add(i);
         }
         
-        return matched;
+        return true;
     }
 }
