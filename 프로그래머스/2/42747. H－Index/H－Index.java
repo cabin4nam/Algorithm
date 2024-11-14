ac9ef8c37e42cdf55b3e81d5d0bc4feb3ef095a2
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-11-14 15:18:09 +0900

    [level 2] Title: H-Index, Time: 0.94 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,27 +1,17 @@
 import java.util.*;
 class Solution {
     public int solution(int[] citations) {
-        int answer = 0;
-        
-        // 3 0 6 1 5
         // 6 5 3 1 0
-        
-        // i=0 -> 6편
-        // i=1 -> 5편
-        // i=2 -> 3번
-        
         Arrays.sort(citations);
         
-        for(int i=0; i<citations.length; i++){
-            // 인용된 논문의 수
-            int count = citations.length - i;
-            
-            if(count <= citations[i]){
-                answer = count;
-                break;
-            }
+        for(int h=citations.length; h>=1; h--){
+            if(isValid(citations, h)) return h;
         }
         
-        return answer;
+        return 0;
+    }
+    private static boolean isValid(int[] citations, int h){
+        int index = citations.length-h;
+        return citations[index] >= h;
     }
 }
