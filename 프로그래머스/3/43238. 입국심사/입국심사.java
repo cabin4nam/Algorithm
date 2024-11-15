4771be80c3d6ce8ad631082e7810b03167214e3c
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-11-15 14:16:32 +0900

    [level 3] Title: 입국심사, Time: 115.69 ms, Memory: 92.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,32 +1,38 @@
-import java.util.Arrays;
-
+import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
         long answer = 0;
         
         Arrays.sort(times);
         
-        // 이분 탐색
-        long left = 1; // 최소시간
-        long right = times[times.length-1] * (long)n; // 최대시간
+        long left = 1;
+        long right = times[times.length-1]*(long)n;
         long mid = 0;
         
         while(left <= right){
             mid = (left+right)/2;
             
-            // mid 시간동안 입국심사관이 심사할 수 있는 사람의 수
-            long count = 0;
-            for(int t : times) count += mid/t;
+            // mid 시간동안 심사할 수 있는 인원 체크
+            long count = canDo(times, mid);
             
-            if(count >= n){ // n명보다 더 많이 심사할 수 있으면 -> 더 짧은 시간으로 조정
+            if(count >= n){
                 answer = mid;
                 right = mid-1;
             }
-            else { // n명보다 적은 사람을 심사할 수 있으면 -> 시간 늘리기
+            else{
                 left = mid+1;
             }
         }
         
         return answer;
+    }
+    
+    private static long canDo(int[] times, long time){
+        long count = 0;
+        for(int t : times){
+            count += time/t;
+        }
+        
+        return count;
     }
 }
