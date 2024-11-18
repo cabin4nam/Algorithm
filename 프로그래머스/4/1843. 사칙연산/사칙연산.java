972dfe0ce990ea5ae2940a0a7623d7f923c502ec
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-11-18 14:22:07 +0900

    [level 4] Title: 사칙연산, Time: 18.83 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.java
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.java
@@ -1,84 +1,52 @@
-import java.util.*;
-import java.lang.*; 
-
 class Solution {
-    int[][] min;
-    int[][] max;
     public int solution(String arr[]) {
-        int answer = -1;
+        int numberCnt = arr.length/2 + 1;
+        int opCnt = arr.length/2;
         
-        // 전체 식에서 피연산자(숫자)의 개수
-        int size = arr.length/2+1; 
+        int[] numbers = new int[numberCnt];
+        String[] ops = new String[opCnt];
         
-        // min[i][j] = i부터 j까지의 결과값의 최소
-        min = new int[size][size];
-        // max[i][j] = i부터 j까지의 결과값이 최대
-        max = new int[size][size];
-        
-        // 전체 식의 피연산자들을 연산자와 합쳐서 (2-1 은 2와 -1로 저장) int 형으로 저장할 배열
-        int[] list = new int[size];
-        for(int i=0; i<arr.length; i+=2){
-            int num = Integer.parseInt(arr[i]);
-            // 0이면 +이든 -이든 상관 없음
-            if(i==0){
-                list[i/2] = num;
-            }
-            else {
-                list[i/2] = arr[i-1].equals("+") ? num : -num;
-            }
+        int idxNum = 0;
+        int idxOp = 0;
+        for(String a : arr){
+            if(a.equals("+") || a.equals("-")) ops[idxOp++] = a; 
+            else numbers[idxNum++] = Integer.parseInt(a);
         }
         
-        for(int i=size-1; i>=0; i--){
-            for(int j = i; j<size; j++){
-                // i와 j가 같은 경우는 숫자 하나인 경우 
-                // -> 최소와 최대가 자기 자신일 수 밖에 없음
-                if(i==j){
-                    min[i][j] = list[i];
-                    max[i][j] = list[i];
-                }
-                else {
-                    // 비교할 수 있게 초기값 설정
-                    min[i][j] = Integer.MAX_VALUE;
-                    max[i][j] = Integer.MIN_VALUE;
+        int[][] maxDP = new int[numberCnt][numberCnt];
+        int[][] minDP = new int[numberCnt][numberCnt];
+    
+        for(int i=0; i<numberCnt; i++) maxDP[i][i] = minDP[i][i] = numbers[i];
+        
+        for(int len = 1; len<numberCnt; len++){
+            for(int start = 0; start<numberCnt; start++){
+                if (start + len >= numberCnt) break;
+                
+                int end = start+len;
+                int max = Integer.MIN_VALUE;
+                int min = Integer.MAX_VALUE;
+                
+                for(int i=start; i<end; i++){
+                    // 덧셈일 떄
+                    if(ops[i].equals("+")){
+                        max = Math.max(max, maxDP[start][i] + maxDP[i+1][end]);
+                        min = Math.min(min, minDP[start][i] + minDP[i+1][end]);
+                    }
                     
-                    for(int k=i; k<j; k++){
-                        boolean value = k==i ? true : false;
-                        
-                        // 모든 경우의 수를 setValue 함수를 통해 검사
-                        // (- 최솟값) + (- 최솟값)
-                        // 최솟값 + 최댓값
-                        // 최댓값 - 최솟값
-                        // 최댓값 + 최댓값
-                        setValue(min[i][k], min[k+1][j], i, j, value);
-                        setValue(min[i][k], max[k+1][j], i, j, value);
-                        setValue(max[i][k], min[k+1][j], i, j, value);
-                        setValue(max[i][k], max[k+1][j], i, j, value);
+                    // 뺄셈일 때
+                    else{
+                        max = Math.max(max, maxDP[start][i] - minDP[i+1][end]);
+                        min = Math.min(min, minDP[start][i] - maxDP[i+1][end]);
                     }
                 }
+                
+                maxDP[start][end] = max;
+                minDP[start][end] = min;
+                
             }
         }
         
-        return max[0][size-1];
-    }
+        return maxDP[0][numberCnt-1];
     
-    // k 기준으로 나눈 두 파트 -> 앞쪽 : front, 뒤쪽 : back
-    public void setValue(int a, int b, int x, int y, boolean value){
-        // value == true : 숫자가 하나인 경우
-        
-        // front가 숫자 하나 이고, 그 숫자가 - 값 일때
-        // - 값인 한 숫자 -> 괄호를 통해 결과를 좌우할 수 있음
-        // a-b의 경우 : -를 밖으로 빼고, 그 뒤를 괄호로 묶으면 더 작은 음수값을 얻을 수도 있음
-        // a+b의 경우 : 괄호를 사용하지 않고 그대로 연산하는 경우
-        if(value && a < 0){
-            min[x][y] = Math.min(min[x][y], Math.min(a-b, a+b));
-            max[x][y] = Math.max(max[x][y], Math.max(a-b, a+b));
-        }
-        // front가 숫자 하나가 아닌 식이거나
-        // front가 숫자 하나이고, 그 숫자가 + 값 일때
-        else {
-            // 앞쪽 값과 뒤쪽 값을 더한 결과와 현재 min,max 값을 비교 -> 갱신
-            min[x][y] = Math.min(min[x][y], a+b);
-            max[x][y] = Math.max(max[x][y], a+b);
-        }
     }
 }
