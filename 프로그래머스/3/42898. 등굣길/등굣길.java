a0d61f06dcbff3f3ca3ba82cbb80294a490bc72c
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-11-18 11:00:11 +0900

    [level 3] Title: 등굣길, Time: 1.11 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,29 +1,36 @@
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        // m이 가로, n이 세로 길이
-        // dp[n][m] = (m,n)까지 갈 수 있는 최단 경로의 개수
-        int[][] dp = new int[n+1][m+1];
-        dp[1][1] = 1;
+        int answer = 0;
+        
+        // 2차원 배열로 dp 저장
+        int[][] map = new int[n+1][m+1];
+        
+        // 물웅덩이 -1로 표시
         for(int[] p : puddles){
-            dp[p[1]][p[0]] = -1;
+            map[p[1]][p[0]] = -1;
         }
+        map[1][1] = 1;
         
         for(int i=1; i<=n; i++){
             for(int j=1; j<=m; j++){
-                if(dp[i][j] == -1) continue;
+                if(map[i][j]==-1) continue;
                 
-                // 오른쪽으로 이동
-                if(j<m && dp[i][j+1] != -1){
-                    dp[i][j+1] = (dp[i][j+1] + dp[i][j])%1000000007;
+                // 오른쪽 이동
+                if(j+1 <= m && map[i][j+1] != -1){
+                    map[i][j+1] = (map[i][j] + map[i][j+1])%1000000007;
                 }
                 
-                // 아래쪽으로 이동
-                if(i<n && dp[i+1][j] != -1){
-                    dp[i+1][j] = (dp[i+1][j] + dp[i][j])%1000000007;
+                // 아래로 이동
+                if(i+1 <= n && map[i+1][j] != -1){
+                    map[i+1][j] = (map[i][j] + map[i+1][j])%1000000007;
                 }
             }
         }
         
-        return dp[n][m];
+        
+        answer = map[n][m];
+        
+        
+        return answer;
     }
 }
