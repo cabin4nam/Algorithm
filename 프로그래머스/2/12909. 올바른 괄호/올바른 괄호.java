a62496bbead69c8f2d794446efa42f1f2816e14a
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-11-19 15:26:18 +0900

    [level 2] Title: 올바른 괄호, Time: 20.05 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,19 +2,18 @@ import java.util.Stack;
 class Solution {
     boolean solution(String s) {
         Stack<Character> stack = new Stack<>();
-        
         for(int i=0; i<s.length(); i++){
-            char c = s.charAt(i);
-            
-            if(c=='('){
-                stack.push(c);
-            }
+            if(stack.isEmpty()) stack.push(s.charAt(i));
+            else if(s.charAt(i) == '(') stack.push('(');
             else {
-                if(stack.isEmpty() || stack.pop()!='(') return false;
+                char c = stack.pop();
+                
+                if(c!='(') return false;
             }
         }
         
-        if(stack.size() > 0) return false;
+        if(!stack.isEmpty()) return false;
+        
         return true;
     }
 }
