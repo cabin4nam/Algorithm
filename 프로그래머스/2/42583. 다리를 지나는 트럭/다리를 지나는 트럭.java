0fa1a589200f3d407aec3555297d8711bbeb7470
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-11-20 11:13:19 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 31.74 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,40 +1,41 @@
 import java.util.*;
-
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<Integer> queue = new LinkedList<>();
-		int sum = 0; // 현재 다리 위의 하중
-		int time = 0;  // 시간
+        int time = 0;
+        
+        Queue<Integer> bridge = new LinkedList<>();
+        int weightSum = 0;
+        for(int truck : truck_weights){
+            if(bridge.isEmpty()){
+                bridge.offer(truck);
+                weightSum += truck;
+            }
+            else{
+                while(true){
+                    // 자리가 없는 경우
+                    if(bridge.size() >= bridge_length){
+                        int temp = bridge.poll();
+                        weightSum -= temp;
+                    }
 
-		for(int i = 0; i < truck_weights.length; i++) { // 향상된 for문을 쓰는게 좋을 것 
-			int truck = truck_weights[i];
-
-			while(true) {
-				// 큐에 아무것도 없는 경우 = 어떠한 트럭도 다리위에 없음 
-				if(queue.isEmpty()) { 
-					queue.add(truck);
-					sum += truck;
-					time++; // 다리에 오를 때만 시간 추가 
-					break;
-				} else if(queue.size() == bridge_length) { // 큐에 다리 길이만큼 트럭이 다 찬 경우 
-					sum -= queue.poll();
-				} else  { // 다리 길이만큼 큐가 차지않았음
-					// weight 값을 넘지 않는 선에서 새로운 트럭을 다리에 올려줌 
-					if(sum + truck <= weight) {
-						queue.add(truck);
-						sum += truck;
-						time++;
-						break;
-					} else { 
-						// 넘는다면 0을 넣어 이미 큐에 있는 트럭이 다리를 건너게 만듬 
-						queue.add(0);
-						time++;
-					}
-				}
-			}
-		}
-
-        // 마지막 트럭에서 반복문이 끝나는데, 마지막 역시 다리 길이만큼 지나가야하기에 + 다리 길이 
-		return time + bridge_length; 
+                    // 자리는 있는데 무게가 안되는 경우
+                    else if(weightSum + truck > weight){
+                        bridge.offer(0);
+                        time++;
+                    }
+                    // 다리에 올릴 수 있는 경우
+                    else{
+                        bridge.offer(truck);
+                        weightSum += truck;
+                        time++;
+                        break;
+                    }
+                }
+            }
+        }
+        
+        time+=bridge_length+1;
+        
+        return time;
     }
 }
