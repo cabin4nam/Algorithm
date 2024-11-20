25c446437398a703ab5c59b345e3d6cd6187b113
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-11-20 10:42:18 +0900

    [level 2] Title: 기능개발, Time: 2.40 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,33 +1,41 @@
-import java.util.*;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.ArrayList;
+
 class Solution {
     public ArrayList<Integer> solution(int[] progresses, int[] speeds) {
         ArrayList<Integer> answer = new ArrayList<>();
         
-        Queue<Integer> q = new LinkedList<>();
-        // 7 3 9
-        int taskCnt = progresses.length;
-        for(int i=0; i<taskCnt; i++){
+        int[] days = new int[progresses.length];
+        for(int i=0; i<progresses.length; i++){
             int remain = 100-progresses[i];
+            int speed = speeds[i];
+            int day = remain/speed;
             
-            if(remain%speeds[i]==0) q.offer(remain / speeds[i]);
-            else q.offer(remain / speeds[i] + 1);
+            if(remain%speed > 0) day++;
+            
+            days[i] = day;
         }
         
-        while(!q.isEmpty()){
-            int now = q.poll();
-            
-            int count =1;
-            while(true){
-                if(q.isEmpty() || q.peek() > now){
-                    break;
+        Queue<Integer> q = new LinkedList<>();
+        for(int d : days){
+            System.out.println();
+            if(q.isEmpty()) q.offer(d);
+            else{
+                int prev = q.peek();
+                
+                // 현재 작업이 완료되기 전에 이전 작업 완료, 바로 배포
+                if(prev < d){ 
+                    answer.add(q.size());
+                    q.clear();
                 }
                 
-                q.poll();
-                count++;
+                // 현재 작업이 완료돼도 이전 작업이 아직 완료되지 않음, 이전 작업 완료 후 같이 배포
+                q.offer(d);
             }
-            
-            answer.add(count);
         }
+        
+        answer.add(q.size());
         
         return answer;
     }
