99eea47091a3b9fde3cbd02a850b2e62cebd9d4c
남가빈 <102659626+cabin4nam@users.noreply.github.com>
2024-11-20 11:50:04 +0900

    [level 3] Title: 순위, Time: 15.20 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,39 +1,59 @@
 class Solution {
+    private static boolean[][] graph;
     public int solution(int n, int[][] results) {
-        int[][] graph = new int[n + 1][n + 1];
-		for (int[] edge : results) {
-			graph[edge[0]][edge[1]] = 1;
-			graph[edge[1]][edge[0]] = - 1;
-		}
-
-		//출발
-		for (int i = 1; i <= n; i++) {
-			//도착
-			for (int j = 1; j <= n; j++) {
-				//거쳐감
-				for (int k = 1; k <= n; k++) {
-					if(graph[i][k] == 1 && graph[k][j] == 1) {
-						graph[i][j] = 1;
-						graph[j][i] = -1;
-					}
-					
-					if(graph[i][k] == -1 && graph[k][j] == -1) {
-						graph[i][j] = -1;
-						graph[j][i] = 1;
-					}
-				}
-			}
-		}
-		
-		int answer = 0;
-		
-		for(int i = 0 ; i <=n; i++) {
-			int count = 0;
-			for (int j = 0; j <= n; j++) {
-				if(graph[i][j] != 0) count++;
-			}
-			if(count == n-1) answer++;
-		}
+        int answer = 0;
+        
+        // 그래프를 사용하여 깊이 탐색을 통해, 각 선수가 이긴 선수, 진 선수의 수를 세기
+        graph = new boolean[n+1][n+1];
+        
+        for(int[] r : results){
+            int p1 = r[0];
+            int p2 = r[1];
+            
+            graph[p1][p2] = true; // p1이 p2를 이겼음 (p1->p2)
+        }
+        
+        for(int p=1; p<=n; p++){
+            // p가 이긴 선수 수 
+            // -1을 하는 이유는 winCount에서 자기 자신(가장 초기 count값 1)의 경우를 제외
+            int win = winCount(p, new boolean[n+1], n)-1;
+            // p가 진 선수 수
+            int lose = loseCount(p, new boolean[n+1], n)-1;
+            
+            // 본인을 이기고 진 선수의 수 = 전체 게임의 수 (1번 선수는 나머지 4명과 게임. 한 명 당 전체 게임의 수 = 4) 라면, 순위를 정할 수 있음
+            if(win+lose+1 == n) answer++;
+        }
+        
+        
         return answer;
+    }
+    
+    private static int winCount(int player, boolean[] visited, int n){
+        int count = 1;
+        
+        for(int i=1; i<=n; i++){
+            // player가 이긴 상대가 아니거나, 이미 방문한 경우라면 continue
+            if(!graph[player][i] || visited[i]) continue;
+            
+            visited[i] = true;
+            count += winCount(i, visited, n); // player가 이긴 상대가 이긴 상대는 몇 명?
+        }
+        
+        return count;
+    }
+    
+    private static int loseCount(int player, boolean[] visited, int n){
+        int count = 1;
+        
+        for(int i=1; i<=n; i++){
+            // player가 진 상대(player를 이긴 상대)가 아니거나, 
+            // 이미 방문한 경우라면 continue
+            if(!graph[i][player] || visited[i]) continue;
+            
+            visited[i] = true;
+            count += loseCount(i, visited, n); // player를 이긴 상대를 이긴 상대는 몇 명?
+        }
+        
+        return count;
     }
 }
